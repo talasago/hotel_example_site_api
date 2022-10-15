8fcceae806ba840ec4365a59de3d180e064a2c68
talasago <42575556+talasago@users.noreply.github.com>
2022-10-15 23:25:37 +0900

    chore: delete nnecessary route

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
-require "rails/all"
+#require "rails/all"
+require 'active_record/railtie'
+require 'action_controller/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: :auth, controllers: {
-        registrations: 'api/v1/registrations',
-        sessions: 'api/v1/sessions'
-      }
-      resources :mypages, controller: 'users', only: [:destroy]
+      mount_devise_token_auth_for(
+        'User',
+        at: :auth,
+        skip: [:passwords],
+        controllers: {
+          registrations: 'api/v1/registrations',
+          sessions: 'api/v1/sessions'
+        }
+      )
+
+      resource :mypage, controller: 'users', only: [:destroy]
     end
   end
 end
