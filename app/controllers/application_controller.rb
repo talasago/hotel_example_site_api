89e893c7d94d852daeca32c100c259e50de693e0
talasago <42575556+talasago@users.noreply.github.com>
2022-10-15 22:56:08 +0900

    chore: Add patch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # Fake module → reason:https://github.com/heartcombo/devise/issues/5443
+  include RackSessionFixController
+
   #TODO:後々必要になりそうなのでコメントとして追加
-  #include DeviseTokenAuth::Concerns::SetUserByToken
   #skip_before_action :verify_authenticity_token
   #helper_method :current_user, :user_signed_in?
 end
