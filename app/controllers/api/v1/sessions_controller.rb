00c33fadbf0da7d5c1d875b63c1152f304bf3de3
talasago <42575556+talasago@users.noreply.github.com>
2022-10-17 22:33:29 +0900

    feat: Add logout function

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::SessionsController < DeviseTokenAuth::SessionsController
+  skip_after_action :reset_session, only: [:destroy]
+
+  def destroy
+    super
+    session["warden.user.user.key"] = nil
+  end
 end
--- spec/requests/api/v1/sessions_api_spec.rb
+++ spec/requests/api/v1/sessions_api_spec.rb
@@ -22,4 +22,33 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
       end
     end
   end
+
+  describe 'logout' do
+    context 'as an authenticated user' do
+      let!(:user) { FactoryBot.attributes_for(:user) }
+      let!(:auth_params) {
+        post '/api/v1/auth', params: { user: user }
+        get_auth_params_from_login_response_headers(response)
+      }
+
+      it 'success logout and APIs require authentication result in a 401 error' do
+        aggregate_failures do
+          delete '/api/v1/auth/sign_out', headers: auth_params
+          expect(response).to have_http_status(:success)
+
+          get '/api/v1/mypage', headers: auth_params
+          expect(response).to have_http_status(401)
+        end
+      end
+    end
+  end
+
+  #FIXME:helper methodにする
+  def get_auth_params_from_login_response_headers(response)
+    {
+      'access-token': response.headers['access-token'],
+      client: response.headers['client'],
+      uid: response.headers['uid']
+    }
+  end
 end
