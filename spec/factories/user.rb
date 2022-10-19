fd4fac59e1b37deb454278f10126716285ac39e2
talasago <42575556+talasago@users.noreply.github.com>
2022-10-19 22:11:23 +0900

    test: Add invalid-case

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     name { 'TestUser' }
     sequence(:email) { |n| "test#{n}@example.com" }
     password { 'password' }
+
+    trait :invalid do
+      name { nil }
+      email { nil }
+      password { nil }
+    end
   end
 end
--- spec/requests/api/v1/registrations_api_spec.rb
+++ spec/requests/api/v1/registrations_api_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Registrations', type: :request do
   describe 'signup' do
-    let(:user) { FactoryBot.attributes_for(:user) }
-
     context 'regist success' do
+      let(:user) { FactoryBot.attributes_for(:user) }
+
       it 'Create a user' do
         expect {
           post '/api/v1/auth', params: { user: user }
@@ -13,6 +13,16 @@ RSpec.describe 'Api::V1::Registrations', type: :request do
         expect(response.content_type).to include('application/json')
         expect(response.headers.keys).to \
           include('access-token', 'uid', 'client', 'expiry', 'token-type')
+      end
+    end
+
+    context 'error due to invalid user' do
+      let(:user) { FactoryBot.attributes_for(:user, :invalid) }
+      it 'not create a user' do
+        expect {
+          post '/api/v1/auth', params: { user: user }
+        }.to_not change(User, :count)
+        expect(response).to_not be_successful
       end
     end
   end
