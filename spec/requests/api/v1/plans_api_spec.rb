343de3978a71c21fb61f34fce8c1592ba04417e9
talasago <42575556+talasago@users.noreply.github.com>
2022-11-01 21:58:12 +0900

    fix(plans_api_spec): fix degre

--- spec/requests/api/v1/plans_api_spec.rb
+++ spec/requests/api/v1/plans_api_spec.rb
@@ -3,52 +3,52 @@ require 'json'
 
 RSpec.describe 'Api::V1::Plans', type: :request do
   describe 'as a not authenticated user' do
-    it 'Only the record "rank is nil" is included in the response' do
+    it 'Only the record "only is nil" is included in the response' do
       get '/api/v1/plans'
       res_body = JSON.parse(response.body)
+      onlys = res_body['plans'].map { |plan| plan['only'] }
 
-      expect(response).to have_http_status(:success)
-      expect(res_body['plans'][0].key?(:rank))
-      res_body['plans'].each do |plan|
-        expect(plan['rank']).to be nil
+      aggregate_failures do
+        expect(response).to have_http_status(:success)
+        expect(res_body['plans'][0].key?(:only))
+        expect(onlys).to include(nil)
+        expect(onlys).to_not include('member', 'premium')
       end
     end
   end
 
   describe 'as an authenticated user' do
-    describe 'rank of user is premium' do
+    describe 'only of user is premium' do
       let(:registed_user1) { FactoryBot.attributes_for(:user, :registed_user1) }
 
-      it 'include all rank in response' do
+      it 'include all only in response' do
         auth_params = sign_in(registed_user1)
 
         get '/api/v1/plans', headers: auth_params
         res_body = JSON.parse(response.body)
+        onlys = res_body['plans'].map { |plan| plan['only'] }
 
         aggregate_failures do
           expect(response).to have_http_status(:success)
-          res_body['plans'].each do |plan|
-            expect(plan['rank']).to eq(nil) | eq('premium') | eq('normal')
-          end
+          expect(onlys).to include(nil, 'premium', 'normal')
         end
       end
     end
 
-    describe 'rank of user is normal' do
+    describe 'only of user is normal' do
       let(:registed_user2) { FactoryBot.attributes_for(:user, :registed_user2) }
 
-      it 'include only rank nil or normal in response' do
+      it 'include only only nil or normal in response' do
         auth_params = sign_in(registed_user2)
 
         get '/api/v1/plans', headers: auth_params
         res_body = JSON.parse(response.body)
+        onlys = res_body['plans'].map { |plan| plan['only'] }
 
         aggregate_failures do
           expect(response).to have_http_status(:success)
-          res_body['plans'].each do |plan|
-            expect(plan['rank']).to eq(nil) | eq('normal')
-            expect(plan['rank']).to_not eq('premium')
-          end
+          expect(onlys).to include(nil, 'normal')
+          expect(onlys).to_not include('premium')
         end
       end
     end
