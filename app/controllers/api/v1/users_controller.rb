90d73d726a10f144371825bdb42c9db40eedbfb9
talasago <42575556+talasago@users.noreply.github.com>
2022-11-09 21:56:29 +0900

    refactor: to_json→as_json

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,12 @@
-require 'json'
-
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_api_v1_user!
   skip_after_action :update_auth_header, only: [:destroy]
 
   def show
-    # 取得する列を指定
-    # FIXME:
-    current_user_data = JSON.parse(
-      current_api_v1_user.to_json(
-        only: [:email, :username, :rank, :address, :tel,
-               :gender, :birthday, :notification]
-      )
+    render json: current_api_v1_user.as_json(
+      only: [:email, :username, :rank, :address, :tel,
+             :gender, :birthday, :notification]
     )
-
-    render json: current_user_data
   end
 
   def destroy
