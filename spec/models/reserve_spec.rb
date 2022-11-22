109700589db16eb2c97a6d208568106379d50117
talasago <42575556+talasago@users.noreply.github.com>
2022-11-22 20:41:18 +0900

    refactor: resereve_spec.rb

--- spec/models/reserve_spec.rb
+++ spec/models/reserve_spec.rb
@@ -25,48 +25,45 @@ RSpec.describe Reserve, type: :model do
   end
 
   describe 'total_bill' do
-    context 'only weekdays' do
-      #HACK:頑張ったけど普通に関数の方が良いかも。letの値を渡せるならば。
-      shared_examples 'right calcurate total_bill' do |hash, expect|
+    shared_examples 'right calcurate total_bill' do |hash, expect|
+      it {
         reserve_monday = Reserve.new(
           plan_id: 0,
-          date: Date.parse('2020-02-02'),
+          date: stay_date,
           **hash
         )
-        it { expect(reserve_monday.calc_total_bill).to eq expect }
+        expect(reserve_monday.calc_total_bill).to eq expect
+      }
+    end
+
+    context 'only weekdays' do
+      let(:stay_date) { Date.parse('2020-02-02') }
+
+      context 'not additional plans' do
+        include_examples 'right calcurate total_bill', { term: 1, head_count: 1 }, 7_000
+        include_examples 'right calcurate total_bill', { term: 2, head_count: 1 }, 14_000
+        include_examples 'right calcurate total_bill', { term: 1, head_count: 2 }, 14_000
+        include_examples 'right calcurate total_bill', { term: 2, head_count: 2 }, 28_000
+        include_examples 'right calcurate total_bill', { term: 3, head_count: 3 }, 63_000
       end
 
-      include_examples 'right calcurate total_bill', {
-        term: 1, head_count: 1, breakfast: false, early_check_in: false, sightseeing: false
-      }, 7_000
-      include_examples 'right calcurate total_bill', {
-        term: 2, head_count: 1, breakfast: false, early_check_in: false, sightseeing: false
-      }, 14_000
-      include_examples 'right calcurate total_bill', {
-        term: 1, head_count: 2, breakfast: false, early_check_in: false, sightseeing: false
-      }, 14_000
-      include_examples 'right calcurate total_bill', {
-        term: 2, head_count: 2, breakfast: false, early_check_in: false, sightseeing: false
-      }, 28_000
-      include_examples 'right calcurate total_bill', {
-        term: 3, head_count: 3, breakfast: false, early_check_in: false, sightseeing: false
-      }, 63_000
-
-      include_examples 'right calcurate total_bill', {
-        term: 1, head_count: 1, breakfast: true, early_check_in: false, sightseeing: true
-      }, 9_000
-      include_examples 'right calcurate total_bill', {
-        term: 2, head_count: 1, breakfast: false, early_check_in: true, sightseeing: false
-      }, 15_000
-      include_examples 'right calcurate total_bill', {
-        term: 1, head_count: 2, breakfast: true, early_check_in: true, sightseeing: false
-      }, 18_000
-      include_examples 'right calcurate total_bill', {
-        term: 2, head_count: 2, breakfast: false, early_check_in: true, sightseeing: true
-      }, 32_000
-      include_examples 'right calcurate total_bill', {
-        term: 3, head_count: 3, breakfast: true, early_check_in: true, sightseeing: true
-      }, 78_000
+      context 'include additional plans' do
+        include_examples 'right calcurate total_bill', {
+          term: 1, head_count: 1, breakfast: true, early_check_in: false, sightseeing: true
+        }, 9_000
+        include_examples 'right calcurate total_bill', {
+          term: 2, head_count: 1, breakfast: false, early_check_in: true, sightseeing: false
+        }, 15_000
+        include_examples 'right calcurate total_bill', {
+          term: 1, head_count: 2, breakfast: true, early_check_in: true, sightseeing: false
+        }, 18_000
+        include_examples 'right calcurate total_bill', {
+          term: 2, head_count: 2, breakfast: false, early_check_in: true, sightseeing: true
+        }, 32_000
+        include_examples 'right calcurate total_bill', {
+          term: 3, head_count: 3, breakfast: true, early_check_in: true, sightseeing: true
+        }, 78_000
+      end
     end
   end
 end
