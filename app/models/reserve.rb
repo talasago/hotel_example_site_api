caa47ab03e9efb6f3be9af332a8046468382f06b
talasago <42575556+talasago@users.noreply.github.com>
2022-11-23 15:35:28 +0900

    feat(Reserve): modify calcurate total_bill
    - add pattern saturday or sanday

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -22,6 +22,14 @@ class Reserve < ApplicationRecord
   # 引数でroom_billを取得するようにする
   def calc_total_bill
     _total_bill = plan.room_bill * head_count * term
+
+    term.times do |num|
+      rest_date = date
+      rest_date += num
+      if rest_date.wday == 0 || rest_date.wday == 6
+        _total_bill += plan.room_bill * 0.25 * head_count
+      end
+    end
     _total_bill += ADDITIONAL_PLAN_PRICE * head_count * term if breakfast
     _total_bill += ADDITIONAL_PLAN_PRICE * head_count if early_check_in
     _total_bill += ADDITIONAL_PLAN_PRICE * head_count if sightseeing
--- spec/models/reserve_spec.rb
+++ spec/models/reserve_spec.rb
@@ -25,19 +25,19 @@ RSpec.describe Reserve, type: :model do
   end
 
   describe 'total_bill' do
-    shared_examples 'right calcurate total_bill' do |hash, expect|
+    shared_examples 'right calcurate total_bill' do |params, expect|
       it {
-        reserve_monday = Reserve.new(
+        reserve = Reserve.new(
           plan_id: 0,
           date: stay_date,
-          **hash
+          **params
         )
-        expect(reserve_monday.calc_total_bill).to eq expect
+        expect(reserve.calc_total_bill).to eq expect
       }
     end
 
     context 'only weekdays' do
-      let(:stay_date) { Date.parse('2020-02-02') }
+      let(:stay_date) { Date.parse('2020-02-03') } # monday
 
       context 'not additional plans' do
         include_examples 'right calcurate total_bill', { term: 1, head_count: 1 }, 7_000
@@ -63,6 +63,36 @@ RSpec.describe Reserve, type: :model do
         include_examples 'right calcurate total_bill', {
           term: 3, head_count: 3, breakfast: true, early_check_in: true, sightseeing: true
         }, 78_000
+      end
+    end
+
+    context 'include saturday or sanday' do
+      let(:stay_date) { Date.parse('2020-02-08') } # saturday
+
+      context 'not additional plans' do
+        include_examples 'right calcurate total_bill', { term: 1, head_count: 1 }, 8_750
+        include_examples 'right calcurate total_bill', { term: 2, head_count: 1 }, 17_500
+        include_examples 'right calcurate total_bill', { term: 1, head_count: 2 }, 17_500
+        include_examples 'right calcurate total_bill', { term: 2, head_count: 2 }, 35_000
+        include_examples 'right calcurate total_bill', { term: 3, head_count: 3 }, 73_500
+      end
+
+      context 'include additional plans' do
+        include_examples 'right calcurate total_bill', {
+          term: 1, head_count: 1, breakfast: true, early_check_in: false, sightseeing: true
+        }, 10_750
+        include_examples 'right calcurate total_bill', {
+          term: 2, head_count: 1, breakfast: false, early_check_in: true, sightseeing: false
+        }, 18_500
+        include_examples 'right calcurate total_bill', {
+          term: 1, head_count: 2, breakfast: true, early_check_in: true, sightseeing: false
+        }, 21_500
+        include_examples 'right calcurate total_bill', {
+          term: 2, head_count: 2, breakfast: false, early_check_in: true, sightseeing: true
+        }, 39_000
+        include_examples 'right calcurate total_bill', {
+          term: 3, head_count: 3, breakfast: true, early_check_in: true, sightseeing: true
+        }, 88_500
       end
     end
   end
