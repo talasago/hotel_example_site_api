1f6eb25eea5b829da49210b32fcd0b8d1e865276
talasago <42575556+talasago@users.noreply.github.com>
2022-11-26 17:08:13 +0900

    feat(Reserve): add validate total_bill

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -5,7 +5,7 @@ class Reserve < ApplicationRecord
   belongs_to :plan
 
   validates :plan_id, presence: true
-  validates :total_bill, presence: true
+  validates :total_bill, presence: true, numericality: true
   validates :date, presence: true
   validates :term, presence: true
   validates :head_count, presence: true
@@ -13,31 +13,34 @@ class Reserve < ApplicationRecord
   validates :contact, presence: true
   validates :email, presence: true, if: -> { contact == 'email' }
   validates :tel, presence: true, if: -> { contact == 'tel' }
+  validate :validate_total_bill
 
   def term_end
     date + term
   end
 
-  def calc_total_bill(room_bill)
-    basic_bill(room_bill) + additional_plan_bill
-  end
-
   private
 
-  def basic_bill(room_bill)
-    [*0..term - 1].map do |add_day|
-      wday = (date + add_day).wday
-      wday == 0 || wday == 6 ?
-        room_bill * head_count * (1 + ADD_ROOM_BILL_RATE_SAT_AND_SUN) :
-        room_bill * head_count
-    end.reduce(:+)
+  def validate_total_bill
+    return if !plan_id.present? && !term.present? && !date.present?
+
+    if total_bill != calc_total_bill
+      errors.add(:total_bill, 'Invalid amounts have been set.')
+    end
   end
 
-  def additional_plan_bill
-    add_bill = 0
-    add_bill += ADDITIONAL_PLAN_PRICE * head_count * term if breakfast
-    add_bill += ADDITIONAL_PLAN_PRICE * head_count if early_check_in
-    add_bill += ADDITIONAL_PLAN_PRICE * head_count if sightseeing
-    add_bill
+  def calc_total_bill
+    total_bill_ = [*0..term - 1].map do |add_day|
+      wday = (date + add_day).wday
+      wday == 0 || wday == 6 ?
+        plan.room_bill * head_count * (1 + ADD_ROOM_BILL_RATE_SAT_AND_SUN) :
+        plan.room_bill * head_count
+    end.reduce(:+)
+
+    total_bill_ += ADDITIONAL_PLAN_PRICE * head_count * term if breakfast
+    total_bill_ += ADDITIONAL_PLAN_PRICE * head_count if early_check_in
+    total_bill_ += ADDITIONAL_PLAN_PRICE * head_count if sightseeing
+
+    total_bill_
   end
 end
--- spec/models/reserve_spec.rb
+++ spec/models/reserve_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Reserve, type: :model do
   it { is_expected.to validate_presence_of :plan_id }
   it { is_expected.to validate_presence_of :total_bill }
+  it { is_expected.to validate_numericality_of :total_bill }
   it { is_expected.to validate_presence_of :date }
   it { is_expected.to validate_presence_of :term }
   it { is_expected.to validate_presence_of :head_count }
@@ -24,14 +25,17 @@ RSpec.describe Reserve, type: :model do
     expect(reserve.term_end).to eq Date.parse('2022-03-03')
   end
 
-  describe 'total_bill' do
-    shared_examples 'right calcurate total_bill' do |params, expect|
+  describe 'validate total_bill' do
+    shared_examples 'Valid when the total_bill is correct' do |params|
+      # expectって名前変かも
       it {
         reserve = Reserve.new(
+          plan_id: 0,
           date: stay_date,
           **params
         )
-        expect(reserve.calc_total_bill(7_000)).to eq expect
+        reserve.valid?
+        expect(reserve.errors[:total_bill]).to_not include('Invalid amounts have been set.')
       }
     end
 
@@ -39,29 +43,29 @@ RSpec.describe Reserve, type: :model do
       let(:stay_date) { Date.parse('2020-02-03') } # monday
 
       context 'not additional plans' do
-        include_examples 'right calcurate total_bill', { term: 1, head_count: 1 }, 7_000
-        include_examples 'right calcurate total_bill', { term: 2, head_count: 1 }, 14_000
-        include_examples 'right calcurate total_bill', { term: 1, head_count: 2 }, 14_000
-        include_examples 'right calcurate total_bill', { term: 2, head_count: 2 }, 28_000
-        include_examples 'right calcurate total_bill', { term: 3, head_count: 3 }, 63_000
+        include_examples 'Valid when the total_bill is correct', { term: 1, head_count: 1, total_bill: 7_000 }
+        include_examples 'Valid when the total_bill is correct', { term: 2, head_count: 1, total_bill: 14_000 }
+        include_examples 'Valid when the total_bill is correct', { term: 1, head_count: 2, total_bill: 14_000 }
+        include_examples 'Valid when the total_bill is correct', { term: 2, head_count: 2, total_bill: 28_000 }
+        include_examples 'Valid when the total_bill is correct', { term: 3, head_count: 3, total_bill: 63_000 }
       end
 
       context 'include additional plans' do
-        include_examples 'right calcurate total_bill', {
-          term: 1, head_count: 1, breakfast: true, early_check_in: false, sightseeing: true
-        }, 9_000
-        include_examples 'right calcurate total_bill', {
-          term: 2, head_count: 1, breakfast: false, early_check_in: true, sightseeing: false
-        }, 15_000
-        include_examples 'right calcurate total_bill', {
-          term: 1, head_count: 2, breakfast: true, early_check_in: true, sightseeing: false
-        }, 18_000
-        include_examples 'right calcurate total_bill', {
-          term: 2, head_count: 2, breakfast: false, early_check_in: true, sightseeing: true
-        }, 32_000
-        include_examples 'right calcurate total_bill', {
-          term: 3, head_count: 3, breakfast: true, early_check_in: true, sightseeing: true
-        }, 78_000
+        include_examples 'Valid when the total_bill is correct', {
+          term: 1, head_count: 1, breakfast: true, early_check_in: false, sightseeing: true, total_bill: 9_000
+        }
+        include_examples 'Valid when the total_bill is correct', {
+          term: 2, head_count: 1, breakfast: false, early_check_in: true, sightseeing: false, total_bill: 15_000
+        }
+        include_examples 'Valid when the total_bill is correct', {
+          term: 1, head_count: 2, breakfast: true, early_check_in: true, sightseeing: false, total_bill: 18_000
+        }
+        include_examples 'Valid when the total_bill is correct', {
+          term: 2, head_count: 2, breakfast: false, early_check_in: true, sightseeing: true, total_bill: 32_000
+        }
+        include_examples 'Valid when the total_bill is correct', {
+          term: 3, head_count: 3, breakfast: true, early_check_in: true, sightseeing: true, total_bill: 78_000
+        }
       end
     end
 
@@ -69,29 +73,29 @@ RSpec.describe Reserve, type: :model do
       let(:stay_date) { Date.parse('2020-02-08') } # saturday
 
       context 'not additional plans' do
-        include_examples 'right calcurate total_bill', { term: 1, head_count: 1 }, 8_750
-        include_examples 'right calcurate total_bill', { term: 2, head_count: 1 }, 17_500
-        include_examples 'right calcurate total_bill', { term: 1, head_count: 2 }, 17_500
-        include_examples 'right calcurate total_bill', { term: 2, head_count: 2 }, 35_000
-        include_examples 'right calcurate total_bill', { term: 3, head_count: 3 }, 73_500
+        include_examples 'Valid when the total_bill is correct', { term: 1, head_count: 1, total_bill: 8_750 }
+        include_examples 'Valid when the total_bill is correct', { term: 2, head_count: 1, total_bill: 17_500 }
+        include_examples 'Valid when the total_bill is correct', { term: 1, head_count: 2, total_bill: 17_500 }
+        include_examples 'Valid when the total_bill is correct', { term: 2, head_count: 2, total_bill: 35_000 }
+        include_examples 'Valid when the total_bill is correct', { term: 3, head_count: 3, total_bill: 73_500 }
       end
 
       context 'include additional plans' do
-        include_examples 'right calcurate total_bill', {
-          term: 1, head_count: 1, breakfast: true, early_check_in: false, sightseeing: true
-        }, 10_750
-        include_examples 'right calcurate total_bill', {
-          term: 2, head_count: 1, breakfast: false, early_check_in: true, sightseeing: false
-        }, 18_500
-        include_examples 'right calcurate total_bill', {
-          term: 1, head_count: 2, breakfast: true, early_check_in: true, sightseeing: false
-        }, 21_500
-        include_examples 'right calcurate total_bill', {
-          term: 2, head_count: 2, breakfast: false, early_check_in: true, sightseeing: true
-        }, 39_000
-        include_examples 'right calcurate total_bill', {
-          term: 3, head_count: 3, breakfast: true, early_check_in: true, sightseeing: true
-        }, 88_500
+        include_examples 'Valid when the total_bill is correct', {
+          term: 1, head_count: 1, breakfast: true, early_check_in: false, sightseeing: true, total_bill: 10_750
+        }
+        include_examples 'Valid when the total_bill is correct', {
+          term: 2, head_count: 1, breakfast: false, early_check_in: true, sightseeing: false, total_bill: 18_500
+        }
+        include_examples 'Valid when the total_bill is correct', {
+          term: 1, head_count: 2, breakfast: true, early_check_in: true, sightseeing: false, total_bill: 21_500
+        }
+        include_examples 'Valid when the total_bill is correct', {
+          term: 2, head_count: 2, breakfast: false, early_check_in: true, sightseeing: true, total_bill: 39_000
+        }
+        include_examples 'Valid when the total_bill is correct', {
+          term: 3, head_count: 3, breakfast: true, early_check_in: true, sightseeing: true, total_bill: 88_500
+        }
       end
     end
   end
