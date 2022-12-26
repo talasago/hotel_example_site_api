26ef19c2beac84cd6269f4d1b610a63bb715cefb
talasago <42575556+talasago@users.noreply.github.com>
2022-12-26 22:04:01 +0900

    refactor: environmentalize allow-origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080"
+    origins ENV['ALLOW_ORIGIN'] || ""
 
     resource "*",
       headers: :any,
