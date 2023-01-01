9c188d1ab1820d43e160ff5b7acbb04fc9bc92e2
talasago <42575556+talasago@users.noreply.github.com>
2023-01-01 14:27:56 +0900

    test: add context
    - add context for 'room_type is null or not null'

--- spec/requests/api/v1/plans_api_spec.rb
+++ spec/requests/api/v1/plans_api_spec.rb
@@ -47,36 +47,37 @@ RSpec.describe 'Api::V1::Plans', type: :request do
   end
 
   describe '/plans/:id' do
-    # TODO:contextにroom_typeを書く
     context 'not authenticated' do
-      it 'success get "plans.only is null"' do
-        get '/api/v1/plans/0'
-        res_body = JSON.parse(response.body, symbolize_names: true)
+      context "room_type isn't null" do
+        it 'success get "plans.only is null"' do
+          get '/api/v1/plans/0'
+          res_body = JSON.parse(response.body, symbolize_names: true)
 
-        aggregate_failures do
-          expect(response).to have_http_status(:success)
-          expect(res_body).to eq(
-            {
-              plan: {
-                plan_id: 0,
-                plan_name: 'お得な特典付きプラン',
-                room_bill: 7000,
-                min_head_count: 1,
-                max_head_count: 9,
-                min_term: 1,
-                max_term: 9
-              },
-              user: nil,
-              room_type: {
-                room_category_type_name: 'スタンダードツイン',
-                room_type_name: 'ツイン',
-                min_capacity: 1,
-                max_capacity: 2,
-                room_size: 18,
-                facilities: ['ユニット式バス・トイレ', '独立洗面台']
+          aggregate_failures do
+            expect(response).to have_http_status(:success)
+            expect(res_body).to eq(
+              {
+                plan: {
+                  plan_id: 0,
+                  plan_name: 'お得な特典付きプラン',
+                  room_bill: 7000,
+                  min_head_count: 1,
+                  max_head_count: 9,
+                  min_term: 1,
+                  max_term: 9
+                },
+                user: nil,
+                room_type: {
+                  room_category_type_name: 'スタンダードツイン',
+                  room_type_name: 'ツイン',
+                  min_capacity: 1,
+                  max_capacity: 2,
+                  room_size: 18,
+                  facilities: ['ユニット式バス・トイレ', '独立洗面台']
+                }
               }
-            }
-          )
+            )
+          end
         end
       end
 
@@ -101,38 +102,40 @@ RSpec.describe 'Api::V1::Plans', type: :request do
           expect(response).to have_http_status(:success)
         end
 
-        it 'success get "plans.only is premium"' do
-          get '/api/v1/plans/1', headers: auth_params
-          res_body = JSON.parse(response.body, symbolize_names: true)
+        context "room_type isn't null" do
+          it 'success get "plans.only is premium"' do
+            get '/api/v1/plans/1', headers: auth_params
+            res_body = JSON.parse(response.body, symbolize_names: true)
 
-          aggregate_failures do
-            expect(response).to have_http_status(:success)
-            expect(res_body).to eq(
-              {
-                plan: {
-                  plan_id: 1,
-                  plan_name: 'プレミアムプラン',
-                  room_bill: 10_000,
-                  min_head_count: 2,
-                  max_head_count: 9,
-                  min_term: 1,
-                  max_term: 9
-                },
-                user: {
-                  user_name: '山田一郎',
-                  tel: '01234567891',
-                  email: 'ichiro@example.com'
-                },
-                room_type: {
-                  room_category_type_name: 'プレミアムツイン',
-                  room_type_name: 'ツイン',
-                  min_capacity: 1,
-                  max_capacity: 3,
-                  room_size: 24,
-                  facilities: ['セパレート式バス・トイレ', '独立洗面台']
+            aggregate_failures do
+              expect(response).to have_http_status(:success)
+              expect(res_body).to eq(
+                {
+                  plan: {
+                    plan_id: 1,
+                    plan_name: 'プレミアムプラン',
+                    room_bill: 10_000,
+                    min_head_count: 2,
+                    max_head_count: 9,
+                    min_term: 1,
+                    max_term: 9
+                  },
+                  user: {
+                    user_name: '山田一郎',
+                    tel: '01234567891',
+                    email: 'ichiro@example.com'
+                  },
+                  room_type: {
+                    room_category_type_name: 'プレミアムツイン',
+                    room_type_name: 'ツイン',
+                    min_capacity: 1,
+                    max_capacity: 3,
+                    room_size: 24,
+                    facilities: ['セパレート式バス・トイレ', '独立洗面台']
+                  }
                 }
-              }
-            )
+              )
+            end
           end
         end
 
@@ -157,31 +160,33 @@ RSpec.describe 'Api::V1::Plans', type: :request do
           expect(response).to have_http_status(401)
         end
 
-        it 'success get "plans.only is normal' do
-          get '/api/v1/plans/3', headers: auth_params
-          res_body = JSON.parse(response.body, symbolize_names: true)
+        context "room_type is null" do
+          it 'success get "plans.only is normal' do
+            get '/api/v1/plans/3', headers: auth_params
+            res_body = JSON.parse(response.body, symbolize_names: true)
 
-          aggregate_failures do
-            expect(response).to have_http_status(:success)
-            expect(res_body).to eq(
-              {
-                plan: {
-                  plan_id: 3,
-                  plan_name: 'お得なプラン',
-                  room_bill: 6000,
-                  min_head_count: 1,
-                  max_head_count: 9,
-                  min_term: 1,
-                  max_term: 9
-                },
-                user: {
-                  user_name: '松本さくら',
-                  tel: nil,
-                  email: 'sakura@example.com',
-                },
-                room_type: nil
-              }
-            )
+            aggregate_failures do
+              expect(response).to have_http_status(:success)
+              expect(res_body).to eq(
+                {
+                  plan: {
+                    plan_id: 3,
+                    plan_name: 'お得なプラン',
+                    room_bill: 6000,
+                    min_head_count: 1,
+                    max_head_count: 9,
+                    min_term: 1,
+                    max_term: 9
+                  },
+                  user: {
+                    user_name: '松本さくら',
+                    tel: nil,
+                    email: 'sakura@example.com',
+                  },
+                  room_type: nil
+                }
+              )
+            end
           end
         end
       end
