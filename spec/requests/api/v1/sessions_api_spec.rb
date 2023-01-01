b33664156bde72cc343cef4ca02aff405604add8
talasago <42575556+talasago@users.noreply.github.com>
2023-01-01 16:26:00 +0900

    test(sessions): refactor

--- spec/requests/api/v1/sessions_api_spec.rb
+++ spec/requests/api/v1/sessions_api_spec.rb
@@ -3,27 +3,25 @@ require 'json'
 
 RSpec.describe 'Api::V1::Sessions', type: :request do
   let(:user) { FactoryBot.attributes_for(:user) }
+  before do
+    @auth_params = sign_up(user)
+  end
 
   describe 'login' do
-    context 'exsist a user' do
-      before do
-        sign_up(user)
-      end
-
-      it 'success authenticate' do
+    context 'exist a user' do
+      it 'successful API call and authentication' do
         post '/api/v1/auth/sign_in', params: {
           email: user[:email],
           password: user[:password]
         }
         expect(response).to have_http_status(:success)
-        expect(response.content_type).to include('application/json')
         expect(response.headers.keys).to \
           include('access-token', 'uid', 'client', 'expiry', 'token-type')
       end
     end
 
     context 'not exist a user' do
-      it 'error login' do
+      it 'failed API call and authentication' do
         post '/api/v1/auth/sign_in', params: {
           email: 'not_exist_user@example.com',
           password: 'password'
@@ -35,16 +33,16 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
 
   describe 'logout' do
     context 'as an authenticated user' do
-      # FIXME:なんか成功なのに401って不安になる書き方。aggregate_failures要らなさそう。
-      it 'success logout and APIs require authentication result in a 401 error' do
-        auth_params = sign_up(user)
-        aggregate_failures do
-          delete '/api/v1/auth/sign_out', headers: auth_params
-          expect(response).to have_http_status(:success)
+      it 'API call succeeds and logout succeeds' do
+        delete '/api/v1/auth/sign_out', headers: @auth_params
+        expect(response).to have_http_status(:success)
+      end
 
-          get '/api/v1/mypage', headers: auth_params
-          expect(response).to have_http_status(401)
-        end
+      it 'after logout, calling an API that requires authentication results in an error' do
+        delete '/api/v1/auth/sign_out', headers: @auth_params
+
+        get '/api/v1/mypage', headers: @auth_params
+        expect(response).to have_http_status(401)
       end
     end
   end
