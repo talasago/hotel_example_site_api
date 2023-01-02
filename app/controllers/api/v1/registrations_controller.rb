b497faca108a27144698d9011dd8f0f640fd1963
talasago <42575556+talasago@users.noreply.github.com>
2023-01-02 20:42:25 +0900

    fix!(/auth):
    Modified parameter contents to be registered at user registration

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,7 +1,3 @@
 class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  private
 
-  def sign_up_params
-    params.permit(:username, :email, :password, :password_confirmation, :member_rank)
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-
+  include Pundit::Authorization
   # Fake module → reason:https://github.com/heartcombo/devise/issues/5443
   include RackSessionFixController
 
-  include Pundit::Authorization
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def pundit_user
     current_api_v1_user
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :username, :rank, :address, :tel, :gender,
+                  :birthday, :notification])
   end
 end
