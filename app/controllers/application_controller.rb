fb8b8228a8d4c9f10093a2afbd56bdd1af020032
talasago <42575556+talasago@users.noreply.github.com>
2023-01-02 12:54:05 +0900

    refactor: delete unnecessary comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,4 @@ class ApplicationController < ActionController::API
   def pundit_user
     current_api_v1_user
   end
-
-  #TODO:後々必要になりそうなのでコメントとして追加
-  #skip_before_action :verify_authenticity_token
-  #helper_method :current_user, :user_signed_in?
 end
