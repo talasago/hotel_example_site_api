fb9213a2762613ef6c6673733ff070c5a7489fca
talasago <42575556+talasago@users.noreply.github.com>
2023-01-03 12:35:24 +0900

    feat(user): add validation of tel, rank, gender

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   validates :username, presence: true
+  validates :tel, allow_nil: true, length: { is: 11 }
+  validates :rank, inclusion: { in: ['premium', 'normal'] }
+  validates :gender, presence: true, inclusion: { in: [0, 1, 2, 9] }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of :username }
   # emailのバリデーションはDeviseTokenAuth::Concerns::Userをincludeした際に追加されている模様
   it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_length_of(:tel).is_equal_to(11).allow_nil }
+  it { is_expected.to validate_inclusion_of(:rank).in_array(['premium', 'normal']) }
+  it { is_expected.to validate_presence_of :gender }
+  it { is_expected.to validate_inclusion_of(:gender).in_array([0, 1, 2, 9]) }
 end
