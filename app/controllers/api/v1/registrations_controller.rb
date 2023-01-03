1fb016ad1aab8fe19ed5352b10612504d85a23a7
talasago <42575556+talasago@users.noreply.github.com>
2023-01-03 10:34:19 +0900

    feat(/auth): modify response body
    - response body to be empty on success

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsController
 
+  # @override
+  def render_create_success
+    render :json
+  end
 end
--- spec/requests/api/v1/registrations_api_spec.rb
+++ spec/requests/api/v1/registrations_api_spec.rb
@@ -6,12 +6,15 @@ RSpec.describe 'Api::V1::Registrations', type: :request do
       let(:user) { FactoryBot.attributes_for(:user) }
 
       it 'successful API call and create a user' do
-        expect {
-          post '/api/v1/auth', params: user
-        }.to change(User, :count).by(1)
-        expect(response).to have_http_status(:success)
-        expect(response.headers.keys).to \
-          include('access-token', 'uid', 'client', 'expiry', 'token-type')
+        aggregate_failures do
+          expect {
+            post '/api/v1/auth', params: user
+          }.to change(User, :count).by(1)
+          expect(response).to have_http_status(:success)
+          expect(response.headers.keys).to \
+            include('access-token', 'uid', 'client', 'expiry', 'token-type')
+          expect(response.body.strip).to eq ''
+        end
       end
     end
 
