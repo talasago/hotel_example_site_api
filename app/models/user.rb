60287c49e15ee730abfaa862ef338e2d52b446bb
talasago <42575556+talasago@users.noreply.github.com>
2023-01-07 11:16:12 +0900

    fix(user): modify validation of email - validation of "email" is the same as "Reserve

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # TODO:↓これ何？
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  # FIXME:Reserveと共通化
+  # NOTE: メールアドレスのバリデーションは、input type="email">と同じとした。
+  # https://developer.mozilla.org/ja/docs/Web/HTML/Element/input/email
+  # https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+  DeviseTokenAuthEmailValidator::EMAIL_REGEXP = /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
 
   validates :username, presence: true
   validates :tel, allow_nil: true, length: { is: 11 }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of :username }
-  # emailのバリデーションはDeviseTokenAuth::Concerns::Userをincludeした際に追加されている模様
-  it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_length_of(:tel).is_equal_to(11).allow_nil }
   it { is_expected.to validate_inclusion_of(:rank).in_array(['premium', 'normal']) }
   it { is_expected.to validate_presence_of :gender }
   it { is_expected.to validate_inclusion_of(:gender).in_array([0, 1, 2, 9]) }
+
+  describe 'email' do
+    subject { User.new }
+    it { is_expected.to validate_presence_of :email }
+
+    context 'format valid' do
+      #FIXME:FactoryBot使った方が良いかも
+      let(:user) { User.new(email: 'example@example.com') }
+      it {
+        user.valid?
+        expect(user.errors[:email]).to eq []
+      }
+    end
+
+    context 'format invalid' do
+      let(:user) { User.new(email: '@') }
+      it {
+        user.valid?
+        expect(user.errors[:email]).to include 'is not an email'
+      }
+    end
+  end
 end
