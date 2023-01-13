4b380572e66465cc0a9352907f0dcf94c769339c
talasago <42575556+talasago@users.noreply.github.com>
2023-01-13 20:55:41 +0900

    feat: Allow login only if not already logged in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::SessionsController < DeviseTokenAuth::SessionsController
   skip_after_action :reset_session, only: [:destroy]
 
+  def create
+    # ログイン済みではない場合にのみログインを許可
+    render status: 401 and return if api_v1_user_signed_in?
+
+    super
+  end
+
   def destroy
     super
     session["warden.user.user.key"] = nil
--- spec/requests/api/v1/sessions_api_spec.rb
+++ spec/requests/api/v1/sessions_api_spec.rb
@@ -8,26 +8,38 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
   end
 
   describe 'login' do
-    context 'exist a user' do
-      it 'successful API call and authentication' do
-        post '/api/v1/auth/sign_in', params: {
-          email: user[:email],
-          password: user[:password]
-        }
-        expect(response).to have_http_status(:success)
-        expect(response.headers.keys).to \
-          include('access-token', 'uid', 'client', 'expiry', 'token-type')
-        expect(response.body.strip).to eq ''
+    context 'not authenticated' do
+      context 'exist a user' do
+        it 'successful API call and authentication' do
+          post '/api/v1/auth/sign_in', params: {
+            email: user[:email],
+            password: user[:password]
+          }
+          expect(response).to have_http_status(:success)
+          expect(response.headers.keys).to \
+            include('access-token', 'uid', 'client', 'expiry', 'token-type')
+          expect(response.body.strip).to eq ''
+        end
+      end
+
+      context 'not exist a user' do
+        it 'failed API call and authentication' do
+          post '/api/v1/auth/sign_in', params: {
+            email: 'not_exist_user@example.com',
+            password: 'password'
+          }
+          expect(response).to have_http_status(401)
+        end
       end
     end
 
-    context 'not exist a user' do
+    context 'as an authenticated user' do
       it 'failed API call and authentication' do
         post '/api/v1/auth/sign_in', params: {
-          email: 'not_exist_user@example.com',
-          password: 'password'
-        }
-        expect(response).to have_http_status(401)
+          email: user[:email],
+          password: user[:password]
+        }, headers: @auth_params
+        expect(response).to have_http_status(401) 
       end
     end
   end
