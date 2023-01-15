baa60c39e6dacaea9e5469e65034eaa4cf0fb942
talasago <42575556+talasago@users.noreply.github.com>
2023-01-15 10:35:19 +0900

    refactor(plan): move the process to private method

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,44 +1,24 @@
 class Api::V1::PlansController < ApplicationController
   def index
-    all_plans = policy_scope(Plan.includes(:room_type)).select(
-      '"plans".id AS plan_id,
-      name AS plan_name,
-      room_bill,
-      min_head_count,
-      "plans".only,
-      room_type_id'
-    ).order(:plan_id)
-
-    return_plans = all_plans.map do |plan|
-      plan.as_json(except: [:id, :room_type_id]).merge(
-        { room_category_type_name: plan.room_type&.room_category_type_name }
-      )
+    return_plans = search_plans.map do |plan|
+      plan.as_json(except: [:id, :room_type_id])
+          .merge({ room_category_type_name: plan.room_type&.room_category_type_name })
     end
 
     render json: { plans: return_plans }
   end
 
   def show
-    #TODO:メソッド化してもいいかも
-    matched_plan = policy_scope(Plan.where(plan_params)).select(
-      '"plans".id AS plan_id,
-      name AS plan_name,
-      room_bill,
-      min_head_count,
-      max_head_count,
-      min_term,
-      max_term,
-      room_type_id'
-    ).first
+    matched_plan = search_plan
 
     render status: 401 and return if matched_plan.nil?
 
     render json: {
       plan: matched_plan.as_json(except: [:id, :room_type_id]),
       user: api_v1_user_signed_in? ? generate_user_hash : nil,
-      room_type: matched_plan.room_type.as_json(except: [:id, :room_category_name])&.merge(
-        { room_category_type_name: matched_plan.room_type&.room_category_type_name }
-      )
+      room_type: matched_plan
+        .room_type.as_json(except: [:id, :room_category_name])
+        &.merge({ room_category_type_name: matched_plan.room_type&.room_category_type_name })
     }
   end
 
@@ -54,5 +34,29 @@ class Api::V1::PlansController < ApplicationController
 
   def plan_params
     params.permit(:id)
+  end
+
+  def search_plan
+    policy_scope(Plan.where(plan_params)).select(
+      '"plans".id AS plan_id,
+      name AS plan_name,
+      room_bill,
+      min_head_count,
+      max_head_count,
+      min_term,
+      max_term,
+      room_type_id'
+    ).first
+  end
+
+  def search_plans
+    policy_scope(Plan.includes(:room_type)).select(
+      '"plans".id AS plan_id,
+      name AS plan_name,
+      room_bill,
+      min_head_count,
+      "plans".only,
+      room_type_id'
+    ).order(:plan_id)
   end
 end
