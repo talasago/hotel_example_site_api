3883539b6e54b25c269966e91a2779fdf741c62a
talasago <42575556+talasago@users.noreply.github.com>
2023-01-26 22:19:26 +0900

    test(sign_in): add test pattern

--- spec/requests/api/v1/sessions_api_spec.rb
+++ spec/requests/api/v1/sessions_api_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 require 'json'
 
 RSpec.describe 'Api::V1::Sessions', type: :request do
+  #FIXME: 変数と前処理を毎回しなくてもいいように
   let(:user) { FactoryBot.attributes_for(:user) }
   before do
     @auth_params = sign_up(user)
@@ -53,6 +54,42 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
             password: user[:password]
           }, headers: auth_params
           expect(response).to have_http_status(401)
+        end
+      end
+
+      context 'post_params is nil' do
+        it 'failed API call' do
+          post '/api/v1/auth/sign_in', headers: @auth_params
+          expect(response).to have_http_status(401)
+        end
+      end
+
+      context 'only unnecessary params exist in request body' do
+        let(:post_params) { generate_unnecessary_params }
+
+        it "failed API call and doesn't create a user" do
+          aggregate_failures do
+            post '/api/v1/auth/sign_in', headers: @auth_params, params: post_params
+            expect(response).to have_http_status(401)
+          end
+        end
+      end
+
+      context 'request body include unnecessary params' do
+        let(:post_params) { {
+          email: user[:email],
+          password: user[:password]
+        }.merge(generate_unnecessary_params) }
+
+        before do
+          delete '/api/v1/auth/sign_out', headers: @auth_params
+        end
+
+        it 'successful API call and create a user' do
+          aggregate_failures do
+            post '/api/v1/auth/sign_in', headers: @auth_params, params: post_params
+            expect(response).to have_http_status(:success)
+          end
         end
       end
     end
