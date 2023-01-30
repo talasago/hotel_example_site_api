893dedf303799825205f73a23306ae93883336eb
talasago <42575556+talasago@users.noreply.github.com>
2023-01-30 22:01:05 +0900

    feat: add render_500_error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :render_500_error
+
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit::Authorization
   # Fake module → reason:https://github.com/heartcombo/devise/issues/5443
   include RackSessionFixController
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def pundit_user
     current_api_v1_user
@@ -17,5 +20,12 @@ class ApplicationController < ActionController::API
       :sign_up, keys: [:email, :password, :username, :rank, :address, :tel, :gender,
                        :birthday, :notification]
     )
+  end
+
+  private
+
+  def render_500_error(error)
+    render json: { errors: [{ message: 'Internal server error.' }] },
+           status: :internal_server_error
   end
 end
