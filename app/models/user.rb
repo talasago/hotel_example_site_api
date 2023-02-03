497ab039652814b01122536a17df12eb6a88fac9
talasago <42575556+talasago@users.noreply.github.com>
2023-02-03 22:04:44 +0900

    style: delete unnecessary comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
-  # TODO:↓これ何？
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
