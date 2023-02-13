3f167bb81f96e2e0e13e9ce2511dc2ee06c34658
talasago <42575556+talasago@users.noreply.github.com>
2023-02-13 20:01:18 +0900

    test(sessions_api_spec): modify
    - fix context
    - fix "before" and "let"
    - fix "context" and "example" linkage

--- spec/requests/api/v1/sessions_api_spec.rb
+++ spec/requests/api/v1/sessions_api_spec.rb
@@ -1,29 +1,43 @@
 require 'json'
 
 RSpec.describe 'Api::V1::Sessions', type: :request do
-  #FIXME: 変数と前処理を毎回しなくてもいいように
   let(:user) { FactoryBot.attributes_for(:user) }
-  before do
-    @auth_params = sign_up(user)
-  end
+  let(:auth_params) { sign_up(user) }
 
   describe 'login' do
-    context 'not authenticated' do
-      context 'exist a user' do
-        it 'successful API call and authentication' do
-          post '/api/v1/auth/sign_in', params: {
-            email: user[:email],
-            password: user[:password]
-          }
-          expect(response).to have_http_status(:success)
-          expect(response.headers.keys).to \
-            include('access-token', 'uid', 'client', 'expiry', 'token-type')
-          expect(response.body.strip).to eq ''
+    let(:post_params) { {
+      email: user[:email],
+      password: user[:password]
+    } }
+
+    context 'when non-authenticated' do
+      context 'when exist a user' do
+        before do
+          sign_up(user)
+        end
+
+        context 'when request body include email and password only' do
+          it 'API call and authentication succeeds' do
+            post '/api/v1/auth/sign_in', params: post_params
+            expect(response).to have_http_status(:success)
+            expect(response.headers.keys).to \
+              include('access-token', 'uid', 'client', 'expiry', 'token-type')
+            expect(response.body.strip).to eq ''
+          end
+        end
+
+        context 'when request body include unnecessary params' do
+          it 'API call successful' do
+            aggregate_failures do
+              post '/api/v1/auth/sign_in', params: post_params.merge(generate_unnecessary_params)
+              expect(response).to have_http_status(:success)
+            end
+          end
         end
       end
 
-      context 'not exist a user' do
-        it 'failed API call and authentication' do
+      context 'when not exist a user' do
+        it 'API call and authentication failed' do
           post '/api/v1/auth/sign_in', params: {
             email: 'not_exist_user@example.com',
             password: 'password'
@@ -33,84 +47,61 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
       end
     end
 
-    context 'as an authenticated user' do
-      context 'authenticated user and user trying to sign in are same' do
-        it 'failed API call and authentication' do
+    context 'when user is authenticated' do
+      context 'when authenticated user and user trying to sign in are same' do
+        it 'API call failed' do
+          post '/api/v1/auth/sign_in', params: post_params, headers: auth_params
+          expect(response).to have_http_status(401)
+        end
+      end
+
+      context 'when authenticated user and user trying to sign in are different' do
+        let(:auth_params_user1) { sign_in(FactoryBot.attributes_for(:user, :registed_user1)) }
+
+        it 'API call failed' do
           post '/api/v1/auth/sign_in', params: {
             email: user[:email],
             password: user[:password]
-          }, headers: @auth_params
+          }, headers: auth_params_user1
           expect(response).to have_http_status(401)
         end
       end
+    end
 
-      context 'authenticated user and user trying to sign in are different' do
-        let(:auth_params) { sign_in(FactoryBot.attributes_for(:user, :registed_user1)) }
+    context 'when post_params is nil' do
+      it 'API call failed' do
+        post '/api/v1/auth/sign_in'
+        expect(response).to have_http_status(401)
+      end
+    end
 
-        it 'failed API call and authentication' do
-          post '/api/v1/auth/sign_in', params: {
-            email: user[:email],
-            password: user[:password]
-          }, headers: auth_params
+    context 'when only unnecessary params exist in request body' do
+      it 'API call failed' do
+        aggregate_failures do
+          post '/api/v1/auth/sign_in', params: generate_unnecessary_params
           expect(response).to have_http_status(401)
-        end
-      end
-
-      context 'post_params is nil' do
-        it 'failed API call' do
-          post '/api/v1/auth/sign_in', headers: @auth_params
-          expect(response).to have_http_status(401)
-        end
-      end
-
-      context 'only unnecessary params exist in request body' do
-        let(:post_params) { generate_unnecessary_params }
-
-        it "failed API call and doesn't create a user" do
-          aggregate_failures do
-            post '/api/v1/auth/sign_in', headers: @auth_params, params: post_params
-            expect(response).to have_http_status(401)
-          end
-        end
-      end
-
-      context 'request body include unnecessary params' do
-        let(:post_params) { {
-          email: user[:email],
-          password: user[:password]
-        }.merge(generate_unnecessary_params) }
-
-        before do
-          delete '/api/v1/auth/sign_out', headers: @auth_params
-        end
-
-        it 'successful API call and create a user' do
-          aggregate_failures do
-            post '/api/v1/auth/sign_in', headers: @auth_params, params: post_params
-            expect(response).to have_http_status(:success)
-          end
         end
       end
     end
   end
 
   describe 'logout' do
-    context 'as an authenticated user' do
+    context 'when user is authenticated' do
       it 'API call succeeds and logout succeeds' do
-        delete '/api/v1/auth/sign_out', headers: @auth_params
+        delete '/api/v1/auth/sign_out', headers: auth_params
         expect(response).to have_http_status(:success)
       end
 
       it 'after logout, calling an API that requires authentication results in an error' do
-        delete '/api/v1/auth/sign_out', headers: @auth_params
+        delete '/api/v1/auth/sign_out', headers: auth_params
 
-        get '/api/v1/mypage', headers: @auth_params
+        get '/api/v1/mypage', headers: auth_params
         expect(response).to have_http_status(401)
       end
     end
 
-    context 'header is null' do
-      it 'failed API call' do
+    context 'when header is null' do
+      it 'API call failed' do
         delete '/api/v1/auth/sign_out'
         expect(response).to have_http_status(404)
       end
