ede8ca534fa672c065f2a30f04a3fe3a4063314c
talasago <42575556+talasago@users.noreply.github.com>
2023-03-02 22:55:37 +0900

    refactor: add DeviseTokenAuthEmailValidator

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  # NOTE:DeviseTokenAuthでemail形式変える方法が他に無さそうだったため、やむなく定数に再代入
-  DeviseTokenAuthEmailValidator::EMAIL_REGEXP = Constants::EMAIL_REGEXP
-
   validates :username, presence: true
   validates :tel, allow_nil: true, length: { is: 11 }
   validates :rank, inclusion: { in: ['premium', 'normal'] }
